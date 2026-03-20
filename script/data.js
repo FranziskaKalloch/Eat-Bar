acb2699f89e72953822ab45fa2c970154650087c
Franziska Kalloch <franzy@AirvonFranziska.fritz.box>
2026-03-20 15:03:50 +0100

    first commit

---
+++ script/data.js
@@ -0,0 +1,8 @@
+// Man soll die Menüs in ein Array packen
+// dafür ist der Name des Gerichts, die Beschreibung und der Preis notwendigt
+// der Preis ist ein Number!
+
+// image source könnte auch noch rein
+
+// amount: 0;  hinzufügen -> Wie oft ist dieses Gericht im Warenkorb
+// hierbei darf ist nicht weniger als 0 sein
