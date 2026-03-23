3ac286c39cded449155378f92e9918e7fe742f7d
Franziska Kalloch <franzy@AirvonFranziska.fritz.box>
2026-03-23 15:04:08 +0100

    content begrenzung

--- script/data.js
+++ script/data.js
@@ -6,3 +6,63 @@
 
 // amount: 0;  hinzufügen -> Wie oft ist dieses Gericht im Warenkorb
 // hierbei darf ist nicht weniger als 0 sein
+
+let burgers = [
+  {
+    name: 'Cheese Burger',
+    description: '120g Beef, Pickles, Smoked cheese, Ketchup',
+    img: './assets/img/cheese-burger.jpg',
+    price: 12.9,
+  },
+
+  {
+    name: 'Crispy Chicken',
+    description: '130g Chicken, Onions, Cheese, Tomatoes, Lettuce',
+    img: './assets/img/crispy-chicken.jpg',
+    price: 12.9,
+  },
+
+  {
+    name: 'Double Beef Burger',
+    description: '300g Beef, Double cheese, Onions, Chili cheese sauce',
+    img: './assets/img/double-beef-cheeseburger.jpg',
+    price: 15.9,
+  },
+
+  {
+    name: 'Premium House Burger',
+    description: '375g Beef, Bacon, Cheddar cheese, Barbecue sauce',
+    img: './assets/img/premium-burger.jpg',
+    price: 17.9,
+  },
+];
+
+let pizzas = [
+  {
+    name: 'Pizza Salami',
+    description: 'Tomato sauce, Salami, Cheese',
+    img: './assets/img/pizza-salami.jpg',
+    price: 12.9,
+  },
+
+  {
+    name: 'Pizza Tomate Mozzarella',
+    description: 'Tomato sauce, Mozzarella',
+    img: './assets/img/tomate-mozarella.jpg',
+    price: 10.9,
+  },
+
+  {
+    name: 'Pizza cheese ground meet',
+    description: 'ground meet, cheese, jalapenos',
+    img: './assets/img/pizza-cheese-ground-meet.jpg',
+    price: 12.9,
+  },
+
+  {
+    name: 'Pizza Quattro Formaggi',
+    description: 'Tomato sauce, Mozzarella, Gorgonzola, Cheddar, Parmigiano Reggiano',
+    img: './assets/img/pizza-vier-käse.jpg',
+    price: 12.9,
+  },
+];
